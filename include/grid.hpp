afdd02419faf9c5b74ec4f5f446a442c8f7b3120
Josie Thompson <josiahst@gmail.com>
2020-06-09 14:49:49 -0700

    added doxygen tags to documentation

--- include/grid.hpp
+++ include/grid.hpp
@@ -9,128 +9,159 @@
 
 namespace hax {
 
-    enum class HexType { Pointed, Flat };
+enum class HexType { Pointed, Flat };
 
-    /** Interface to a hexagonal grid.
+/**
+ * Interface to a hexagonal grid.
+ */
+class Grid {
+    std::valarray<float> _origin;
+    float _unit_size;
+    float _offset;
+    std::valarray<float> basis;
+    std::valarray<float> inv_basis;
+public:
+    // TODO: redesign constructors to be more uniform
+
+    /**
+     * Construct a grid with a global origin, unit size and offset angle.
+     *
+     * \param x the x-coordinate of the cartesian origin.
+     * \param y the y-coordinate of the cartesian origin.
+     * \param size the unit size of the grid, a positive number.
+     * \param th the offset angle of the grid in radians.
+     *
+     * \throws std::invalid_argument if size <= 0.
      */
-    class Grid {
-        std::valarray<float> _origin;
-        float _unit_size;
-        float _offset;
-        std::valarray<float> basis;
-        std::valarray<float> inv_basis;
-    public:
-        /** Construct a grid with a given origin, unit size and offset angle.
-         *
-         * Parameters:
-         *  x, y - the cartesian origin of the grid (in global space)
-         *  size - the unit size of the grid
-         *  th   - the offset angle of the grid
-         *
-         * Throws:
-         *  error if size <= 0
-         */
-        Grid(float, float, float, float);
+    Grid(float, float, float, float);
 
-        /** Construct a grid with a given unit size and offset angle.
-         *
-         * Parameters:
-         *  size - the unit size of the grid
-         *  th   - the offset angle of the grid
-         *
-         * Throws:
-         *  error if size <= 0
-         */
-        Grid(float, float);
+    /**
+     * Construct a grid with a given unit size and offset angle.
+     *
+     * \param size the unit size of the grid, a positive number.
+     * \param th the offset angle of the grid in radians.
+     *
+     * \throws std::invalid_argument if size <= 0.
+     */
+    Grid(float, float);
 
-        /** Construct a grid with a given unit size and hex type.
-         *
-         * Parameters:
-         *  size    - the unit size of the grid
-         *  hextype - the type of hex unit
-         *
-         * Throws:
-         *  error if size <= 0
-         */
-        Grid(float, HexType);
+    /**
+     * Construct a grid with a given unit size and hex type.
+     *
+     * \param size the unit size of the grid, a positive number.
+     * \param hextype the offset angle of the grid as a HexType
+     *
+     * \throws std::invalid_argument if size <= 0.
+     */
+    Grid(float, HexType);
 
-        /** Get or set the unit size of the grid.
-         *
-         * Parameters:
-         *  size - of the grid; must be positive
-         *
-         * Getting the unit_size returns a float.
-         *
-         * Throws:
-         *  error if size <= 0
-         */
-        float unit_size() const noexcept;
-        void unit_size(float);
+    /**
+     * Get the unit size of this grid.
+     *
+     * \return a positive number represing this grid's unit size.
+     */
+    float unit_size() const noexcept;
 
-        /** Get or set the offset angle of the grid.
-         *
-         * Parameters
-         *  th - the offset angle
-         *
-         * Getting the offset returns a float.
-         */
-        float offset() const noexcept;
-        void offset(float) noexcept;
+    // TODO: grid class should be immutable
+    /**
+     * Set the unit size of this grid
+     *
+     * \param size the unit size of the grid, a positive number.
+     * \post change this grid's unit size to size.
+     * \throws std::invalid_argument if size <= 0.
+     */
+    void unit_size(float);
 
-        /** Get or set the origin of the grid.
-         *
-         * Parameters:
-         *
-         * origin(float x, float y)
-         *  x, y - the new origin
-         *
-         * origin(const Point& p)
-         *  p - the new origin
-         *
-         * Getting the offset returns a Point object.
-         */
-        Point origin() const noexcept;
-        void origin(float, float) noexcept;
-        void origin(const Point&) noexcept;
+    /**
+     * Get the offset angle of this grid.
+     *
+     * \return the offset angle of this grid in radians.
+     */
+    float offset() const noexcept;
 
-        /** Convert a hex coordinate to cartesian (in global space)
-         *
-         * Parameters:
-         *  h - the hex coordinate to convert
-         */
-        Point hex_to_cartesian(const Hex&) const noexcept;
+    // TODO: delete: grid class should be immutable
+    /**
+     * Set the offset angle of this grid.
+     *
+     * \param theta the offset angle in radians.
+     * \post change this grid's offset angle to theta.
+     */
+    void offset(float) noexcept;
 
-        /** Convert a cartesian coordinate (in global space) to a hex coordinate
-         *
-         * Parameters:
-         *  x, y - the coordinate to convert
-         */
-        Hex cartesian_to_hex(float x, float y) const noexcept;
+    /**
+     * Get the global origin of this grid.
+     *
+     * \return the cartesian origin of this grid in global space.
+     */
+    Point origin() const noexcept;
 
-        /** Convert a cartesian coordinate (in global space) to a hex coordinate
-         *
-         * Parameters:
-         *  p - the coordinate to convert
-         */
-        Hex cartesian_to_hex(const Point& p) const noexcept;
+    // TODO: delete: Grid class should be immutable
+    /**
+     * Set the origin of this grid.
+     *
+     * \param x the x-component of the cartesian origin.
+     * \param y the y-component of the cartesian origin.
+     * \post change this grid's origin to (x, y)
+     */
+    void origin(float, float) noexcept;
 
-        /** Give the cartesian coordinates of each vertex of a hex.
-         *
-         * Parameters:
-         *  h - the hex coordinate to get the vertices of
-         */
-        std::vector<Point> vertices(const Hex&) const noexcept;
+    // TODO: delete: Grid class should be immutable
+    /**
+     * Set the origin of this grid.
+     *
+     * \param p the cartesian origin.
+     * \post change this grid's origin to p
+     */
+    void origin(const Point&) noexcept;
 
-        /** Create a hexagonal map of hexes.
-         *
-         * Parameters:
-         *  radius - the radius of the map.
-         *
-         * Throws:
-         *  error if radius is less than zero.
-         */
-        static std::unordered_set<Hex> hex_map(int);
-    };
+    /**
+     * Convert a hex coordinate to cartesian.
+     *
+     * \param h the hex coordinate to convert.
+     * \return the cartesian representation of the hex coordinate
+     *         with respect to global origin.
+     */
+    Point hex_to_cartesian(const Hex&) const noexcept;
+
+    /**
+     * Convert a cartesian coordinate to a hex coordinate
+     *
+     * \param x the x-component of the cartesian coordinate in global space.
+     * \param y the y-component of the cartesian coordinate in global space.
+     *
+     * \return the hex representation of (x, y) with respect to this grid's
+     *         origin.
+     */
+    Hex cartesian_to_hex(float x, float y) const noexcept;
+
+    /**
+     * Convert a cartesian coordinate to a hex coordinate.
+     *
+     * \param p the cartesian coordinate in global space.
+     * \return the hex representation of p with respect to this grid's origin.
+     */
+    Hex cartesian_to_hex(const Point& p) const noexcept;
+
+    /**
+     * Calculate the cartesian coordinates of each vertex of a hex.
+     *
+     * \param h the hex coordinate to calculate the vertices of.
+     * \return a list of cartesian coordinates with respect to global origin
+     *         representing the vertices of this hex coordinate. Vertices appear
+     *         in clockwise order starting from upper-right-most vertex.
+     */
+    std::vector<Point> vertices(const Hex&) const noexcept;
+
+    // TODO: delete: a pointless function? or at least rename
+    /**
+     * Create a hexagonal map of hexes.
+     *
+     * \param r the radius in hex units of the map.
+     * \return a set of all hex coordinates in the map.
+     * \throws std::illegal_argument if r < 0
+     */
+    static std::unordered_set<Hex> hex_map(int);
+};
 }
 
 #endif
--- include/point.hpp
+++ include/point.hpp
@@ -4,14 +4,33 @@
 #include <ostream>
 
 namespace hax {
-    struct Point {
-        float x, y;
-        Point(float x, float y);
-        float magnitude() const noexcept;
-    };
 
-    Point operator-(const Point&, const Point&);
-    std::ostream& operator<<(std::ostream&, const Point&);
+// TODO: make field type a generic type
+struct Point {
+    // TODO: make private:
+    //  Point should be immutable and prevent rep exposure
+    float x, y;
+
+    /**
+     * Create a representation of a cartesian point at (x, y)
+     *
+     * \param x the x-component of the cartesian point.
+     * \param y the y-component of the cartesian point.
+     */
+    Point(float x, float y);
+    // TODO: rename to norm, or overload std::abs
+    /**
+     * Calculate the norm of this point.
+     *
+     * \return the 2D euclidean norm of this point.
+     */
+    float magnitude() const noexcept;
+};
+
+// TODO: document?
+Point operator-(const Point&, const Point&);
+std::ostream& operator<<(std::ostream&, const Point&);
+
 }
 
 #endif
