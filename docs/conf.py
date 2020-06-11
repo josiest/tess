c675790aa9e2c52b65de85b4a6bd4a0be8f4e040
Josie Thompson <josiahst@gmail.com>
2020-06-10 23:17:59 -0700

    sphinx docs generalized

--- docs/conf.py
+++ docs/conf.py
@@ -27,11 +27,11 @@ author = 'Josie Thompson'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['breathe']
+breathe_default_project = 'hax'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,4 +49,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
