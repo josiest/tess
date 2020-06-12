9f9782d6186deca54164e782563ed32343375e6b
Josie Thompson <josiahst@gmail.com>
2020-06-12 15:04:09 -0700

    point is now completely template, with robust unit tests

--- include/point.hpp
+++ include/point.hpp
@@ -2,6 +2,9 @@
 #define HEXES_POINT_H
 
 #include <ostream>
+#include <type_traits>
+#include <stdexcept>
+#include <cmath>
 
 namespace hax {
 
@@ -11,8 +14,8 @@ namespace hax {
  * Points may be printed, added, subtracted and negated:
  *
  * \code{.cpp}
- * Point const p1(1, 2);
- * Point const p2(3, 4);
+ * Point<int> const p1(1, 2);
+ * Point<int> const p2(3, 4);
  *
  * std::cout << (p1+p2) << '\n';
  * std::cout << (p1-p2) << '\n';
@@ -20,51 +23,106 @@ namespace hax {
  * \endcode
  *
  * ```
- * > (4.0, 6.0)
- * > (-2.0, -2.0)
- * > (-1.0, -2.0)
+ * > (4, 6)
+ * > (-2, -2)
+ * > (-1, -2)
  * ```
+ *
+ * Note that the generic argument must be a numeric type.
+ * That is, it must satisfy
+ *
+ * \code{.cpp}
+ * std::is_arithmetic<T>::value
+ * \endcode
  */
-class Point {
-    float const _x;
-    float const _y;
+template<typename T>
+    class Point {
+        T const _x;
+        T const _y;
+    
+    public:
+        /**
+         * Create a representation of a cartesian point at (x, y)
+         *
+         * \param x the x-component of the cartesian point.
+         * \param y the y-component of the cartesian point.
+         *
+         * \throws std::invalid_argument if x or y are NaN
+         */
+        Point(T x, T y) :_x(x), _y(y)
+        {
+            static_assert(std::is_arithmetic<T>::value,
+                          "T must be a numeric type");
 
-public:
-    /**
-     * Create a representation of a cartesian point at (x, y)
-     *
-     * \param x the x-component of the cartesian point.
-     * \param y the y-component of the cartesian point.
-     */
-    Point(float x, float y);
-
-    /**
-     * Get the x-component.
-     *
-     * \return the x-component of this point.
-     */
-    inline float x() const noexcept { return _x; }
-
-    /**
-     * Get the y-component.
-     *
-     * \return the y-component of this point.
-     */
-    inline float y() const noexcept { return _y; }
-};
+            if (std::isnan(x)) {
+                throw std::invalid_argument{"x must be a valid number"};
+            }
+            if (std::isnan(y)) {
+                throw std::invalid_argument{"y must be a valid number"};
+            }
+        }
+    
+        /**
+         * Get the x-component.
+         *
+         * \return the x-component of this point.
+         */
+        inline T x() const noexcept { return _x; }
+    
+        /**
+         * Get the y-component.
+         *
+         * \return the y-component of this point.
+         */
+        inline T y() const noexcept { return _y; }
+    };
+    
+/**
+ * Calculate the norm of p.
+ *
+ * \return the 2D euclidean norm of p as a double.
+ */
+template<typename T>
+    double norm(const Point<T>& p) noexcept
+    {
+        return std::sqrt(p.x()*p.x() + p.y()*p.y());
+    }
 
 /**
  * Calculate the norm of p.
  *
- * \return the 2D euclidean norm of p.
+ * \return the 2D euclidean norm of p as a float.
  */
-float norm(const Point& p) noexcept;
+template<typename T>
+    double normf(const Point<T>& p) noexcept
+    {
+        return sqrtf(p.x()*p.x() + p.y()*p.y());
+    }
 
-Point operator+(const Point&, const Point&);
-Point operator-(const Point&);
-Point operator-(const Point&, const Point&);
-std::ostream& operator<<(std::ostream&, const Point&);
+template<typename T>
+    Point<T> operator+(const Point<T>& p, const Point<T>& q)
+    {
+        return Point<T>(p.x()+q.x(), p.y()+q.y());
+    }
 
+template<typename T>
+    Point<T> operator-(const Point<T>& p)
+    {
+        return Point<T>(-p.x(), -p.y());
+    }
+
+template<typename T>
+    Point<T> operator-(const Point<T>& p, const Point<T>& q)
+    {
+        return p + (-q);
+    }
+
+template<typename T>
+    std::ostream& operator<<(std::ostream& os, const Point<T>& p)
+    {
+        os << "(" << p.x() << ", " << p.y() << ")";
+        return os;
+    }
 }
 
 #endif
--- test/test_point.cpp
+++ test/test_point.cpp
@@ -1,247 +1,290 @@
 #include "gtest/gtest.h"
 #include "hax.hpp"
-#include <random>
 #include <limits>
+#include <random>
+#include <cstdlib>
 
 using namespace hax;
 using namespace std;
 
-TEST(PointTest, GetXBothComponentsZero) {
-    Point const p(0, 0);
-    EXPECT_EQ(p.x(), 0);
+TEST(PointTest, BothComponentsZero) {
+    double const eps = 0.0001;
+    float const epsf = 0.0001f;
+
+    Point<int> const ip(0, 0);
+    EXPECT_EQ(ip.x(), 0);
+    EXPECT_EQ(ip.y(), 0);
+    EXPECT_EQ(norm(ip), 0.0);
+    EXPECT_EQ(normf(ip), 0.0f);
+
+    auto const ipn = -ip;
+    EXPECT_EQ(ipn.x(), 0);
+    EXPECT_EQ(ipn.y(), 0);
+    EXPECT_EQ(norm(ipn), 0.0);
+
+    auto const ipa = ip+ip;
+    EXPECT_EQ(ipa.x(), 0);
+    EXPECT_EQ(ipa.y(), 0);
+    EXPECT_EQ(normf(ipa), 0.0f);
+
+    auto const ips = ip-ip;
+    EXPECT_EQ(ips.x(), 0);
+    EXPECT_EQ(ips.y(), 0);
+    EXPECT_EQ(norm(ips), 0.0);
+
+    Point<double> const dp(0.0, 0.0);
+    EXPECT_EQ(dp.x(), 0.0);
+    EXPECT_EQ(dp.y(), 0.0);
+    EXPECT_EQ(norm(dp), 0.0);
+    EXPECT_EQ(normf(dp), 0.0f);
+
+    auto const dpn = -dp;
+    EXPECT_LT(abs(dpn.x()), eps);
+    EXPECT_LT(abs(dpn.y()), eps);
+    EXPECT_GE(normf(dpn), 0.0f);
+    EXPECT_LT(normf(dpn), epsf);
+
+    auto const dpa = dp+dp;
+    EXPECT_LT(abs(dpa.x()), eps);
+    EXPECT_LT(abs(dpa.y()), eps);
+    EXPECT_GE(norm(dpa), 0.0);
+    EXPECT_LT(norm(dpa), eps);
 }
 
-TEST(PointTest, GetXAsZeroWithYPositiveInt) {
-    Point const p1(0, 1);
-    Point const p2(0, 2);
-    Point const p3(0, 11);
-    Point const p4(0, 7821);
+TEST(PointTest, OneComponentZeroOtherPositiveAndSmall) {
+    double const eps = 0.0001;
 
-    EXPECT_EQ(p1.x(), 0);
-    EXPECT_EQ(p2.x(), 0);
-    EXPECT_EQ(p3.x(), 0);
-    EXPECT_EQ(p4.x(), 0);
-}
-
-TEST(PointTest, GetXAsZeroWithYPositiveFloat) {
-    Point const p5(0, 0.0001);
-    Point const p6(0, 0.7632);
-    Point const p7(0, 13.282);
-
-    EXPECT_EQ(p5.x(), 0);
-    EXPECT_EQ(p6.x(), 0);
-    EXPECT_EQ(p7.x(), 0);
-}
-
-TEST(PointTest, GetXAsZeroWithYNegativeInt) {
-    Point const p1(0, -1);
-    Point const p2(0, -3);
-    Point const p3(0, -8);
-    Point const p4(0, -28811);
-
-    EXPECT_EQ(p1.x(), 0);
-    EXPECT_EQ(p2.x(), 0);
-    EXPECT_EQ(p3.x(), 0);
-    EXPECT_EQ(p4.x(), 0);
-}
-
-TEST(PointTest, GetXAsZeroWithYNegativeFloat) {
-    Point const p5(0, -0.00002);
-    Point const p6(0, -0.582);
-    Point const p7(0, -281.23);
-
-    EXPECT_EQ(p5.x(), 0);
-    EXPECT_EQ(p6.x(), 0);
-    EXPECT_EQ(p7.x(), 0);
-}
-
-TEST(PointTest, GetXAsPositiveInt) {
-    Point const p1(1, 0);
-    Point const p2(2, -1);
-    Point const p3(13, 781);
-    Point const p4(82739, -13);
-
-    EXPECT_EQ(p1.x(), 1);
-    EXPECT_EQ(p2.x(), 2);
-    EXPECT_EQ(p3.x(), 13);
-    EXPECT_EQ(p4.x(), 82739);
-}
-
-TEST(PointTest, GetXAsPositiveFloat) {
-    Point const p1(0.00002, 830.0);
-    Point const p2(0.123, 0.0);
-    Point const p3(1.0, -13.3);
-    Point const p4(1332.31313, -2991.23);
-
-    EXPECT_LT(p1.x()-0.00002, 0.000001);
-    EXPECT_LT(p2.x()-0.123, 0.0001);
-    EXPECT_LT(p3.x()-1.0, 0.01);
-    EXPECT_LT(p4.x()-1332.31313, 0.001);
-}
-
-TEST(PointTest, GetXAsNegativeInt) {
-    Point const p1(-1, 13);
-    Point const p2(-2, -2003);
-    Point const p3(-82, -293);
-    Point const p4(-12920, 32);
-
-    EXPECT_EQ(p1.x(), -1);
-    EXPECT_EQ(p2.x(), -2);
-    EXPECT_EQ(p3.x(), -82);
-    EXPECT_EQ(p4.x(), -12920);
-}
-
-TEST(PointTest, GetYBothComponentsZero) {
-    Point const p(0, 0);
-    EXPECT_EQ(p.y(), 0);
-}
-
-TEST(PointTest, GetYAsZeroWithXPositiveInt) {
     random_device rd;
+    uniform_int_distribution<> idist(2, 9);
 
-    Point const p1(1, 0);
-    EXPECT_EQ(p1.y(), 0);
+    int const iy1 = idist(rd);
+    Point<int> const ip1(0, iy1);
 
-    uniform_int_distribution<> dist2(2, 100);
-    Point const p2(dist2(rd), 0);
-    EXPECT_EQ(p2.y(), 0);
+    EXPECT_EQ(ip1.x(), 0);
+    EXPECT_EQ(ip1.y(), iy1);
+    EXPECT_LT(abs(norm(ip1)-iy1), eps);
+    EXPECT_LT(abs(normf(ip1)-iy1), eps);
 
-    uniform_int_distribution<> dist3(101, numeric_limits<int>::max()-1);
-    Point const p3(dist3(rd), 0);
-    EXPECT_EQ(p3.y(), 0);
+    auto const ip1n = -ip1;
+    EXPECT_EQ(ip1n.x(), 0);
+    EXPECT_EQ(ip1n.y(), -iy1);
+    EXPECT_GE(norm(ip1n), 0.0);
+    EXPECT_LT(abs(norm(ip1n)-iy1), eps);
 
-    Point const p4(numeric_limits<int>::max(), 0);
-    EXPECT_EQ(p4.y(), 0);
+    int const iy2 = idist(rd);
+    Point<int> const ip2(0, iy2);
+    auto const ip21s = ip2-ip1;
+
+    EXPECT_EQ(ip21s.x(), 0);
+    EXPECT_EQ(ip21s.y(), iy2-iy1);
+    EXPECT_GE(normf(ip21s), 0.0f);
+    EXPECT_LT(abs(normf(ip21s)-abs(iy2-iy1)), eps);
+
+    uniform_real_distribution<> rdist(1.0001, 9.9999);
+
+    double const rx1 = rdist(rd);
+    Point<double> const rp1(rx1, 0.0);
+
+    EXPECT_LT(rp1.x()-rx1, eps);
+    EXPECT_EQ(rp1.y(), 0.0);
+    EXPECT_LT(abs(normf(rp1)-rx1), eps);
+
+    double const rx2 = rdist(rd);
+    Point<double> const rp2(rx2, 0.0);
+    auto const rp12a = rp1+rp2;
+
+    EXPECT_LT(rp12a.x()-(rx1+rx2), eps);
+    EXPECT_EQ(rp12a.y(), 0.0);
+    EXPECT_LT(abs(norm(rp12a)-(rx1+rx2)), eps);
+
+    auto const rp12s = rp1-rp2;
+    EXPECT_LT(rp12s.x()-abs(rx1-rx2), eps);
+    EXPECT_EQ(rp12s.y(), 0.0);
+    EXPECT_LT(abs(norm(rp12s)-abs(rx1-rx2)), eps);
 }
 
-TEST(PointTest, GetYAsZeroWithXPositiveFloat) {
-    random_device rd;
+TEST(PointTest, BothComponentsPositiveAndLargeWithSmallDifference) {
+    double eps = 0.0001;
+    random_device seed;
 
-    Point const p1(1.0, 0);
-    EXPECT_EQ(p1.y(), 0);
+    uniform_int_distribution<short> idist(20, 99);
+    uniform_int_distribution<short> idiff_dist(-9, 9);
 
-    uniform_real_distribution<float> dist2(0.000001, 0.00001);
-    Point const p2(dist2(rd), 0);
-    EXPECT_EQ(p2.y(), 0);
+    short const ix1 = idist(seed);
+    short const iy1 = ix1 + idiff_dist(seed);
+    double const inorm1 = sqrt(ix1*ix1 + iy1*iy1);
+    Point<short> const ip1(ix1, iy1);
 
-    uniform_real_distribution<float> dist3(0.001, 0.999);
-    Point const p3(dist3(rd), 0);
-    EXPECT_EQ(p3.y(), 0);
+    EXPECT_EQ(ip1.x(), ix1);
+    EXPECT_EQ(ip1.y(), iy1);
+    EXPECT_GE(normf(ip1), 0.0f);
+    EXPECT_LT(abs(normf(ip1)-inorm1), eps);
 
-    uniform_real_distribution<float> dist4(1.001, 99.99);
-    Point const p4(dist4(rd), 0);
-    EXPECT_EQ(p4.y(), 0);
+    auto const ip1n = -ip1;
+    EXPECT_EQ(ip1n.x(), -ix1);
+    EXPECT_EQ(ip1n.y(), -iy1);
+    EXPECT_GE(norm(ip1n), 0.0);
+    EXPECT_LT(abs(norm(ip1n)-inorm1), eps);
 
-    uniform_real_distribution<float>
-        dist5(100.0, numeric_limits<float>::max()-1);
+    short const ix2 = ix1 + idiff_dist(seed);
+    short const iy2 = iy1 + idiff_dist(seed);
+    double const inorm12a = sqrt((ix1+ix2)*(ix1+ix2) + (iy1+iy2)*(iy1+iy2));
 
-    Point const p5(dist5(rd), 0);
-    EXPECT_EQ(p5.y(), 0);
+    Point<short> const ip2(ix2, iy2);
+    auto const ip12a = ip1+ip2;
+    EXPECT_EQ(ip12a.x(), ix1+ix2);
+    EXPECT_EQ(ip12a.y(), iy1+iy2);
+    EXPECT_GE(norm(ip12a), 0.0);
+    EXPECT_LT(abs(norm(ip12a)-inorm12a), eps);
 
-    Point const p6(numeric_limits<float>::max(), 0);
-    EXPECT_EQ(p6.y(), 0);
+    uniform_real_distribution<float> rdist(20.01, 99.99);
+    uniform_real_distribution<float> rdiff_dist(-9.99, 9.99);
+
+    float const ry1 = rdist(seed);
+    float const rx1 = ry1 + rdiff_dist(seed);
+    double const rnorm1 = sqrt(rx1*rx1 + ry1*ry1);
+    Point<float> const rp1(rx1, ry1);
+
+    EXPECT_LT(rp1.x()-rx1, eps);
+    EXPECT_LT(rp1.y()-ry1, eps);
+    EXPECT_GE(normf(rp1), 0.0f);
+    EXPECT_LT(abs(normf(rp1)-rnorm1), eps);
+
+    float const rx2 = rx1 + rdiff_dist(seed);
+    float const ry2 = ry2 + rdiff_dist(seed);
+    Point<float> const rp2(rx2, ry2);
+
+    double const rnorm12a = sqrt((rx1+rx2)*(rx1+rx2) + (ry1+ry2)*(ry1+ry2));
+    auto const rp12a = rp1 + rp2;
+
+    EXPECT_LT(abs(rp12a.x()-(rx1+rx2)), eps);
+    EXPECT_LT(abs(rp12a.y()-(ry1+ry2)), eps);
+    EXPECT_GE(normf(rp12a), 0.0f);
+    EXPECT_LT(abs(normf(rp12a)-rnorm12a), eps);
+
+    double const rnorm21s = sqrt((rx2-rx1)*(rx2-rx1) + (ry2-ry1)*(ry2-ry1));
+    auto const rp21s = rp2 - rp1;
+    EXPECT_LT(abs(rp21s.x()-(rx2-rx1)), eps);
+    EXPECT_LT(abs(rp21s.y()-(ry2-ry1)), eps);
+    EXPECT_GE(norm(rp21s), 0.0);
+    EXPECT_LT(abs(norm(rp21s)-rnorm21s), eps);
 }
 
-TEST(PointTest, GetYAsZeroWithXNegativeInt) {
-    random_device rd;
+TEST(PointTest, XVeryLargeAndNegativeYVeryLargeAndPositive) {
+    random_device seed;
 
-    Point const p1(-1, 0);
-    EXPECT_EQ(p1.y(), 0);
+    uniform_int_distribution<long>
+        xdist(numeric_limits<long>::min()/2, numeric_limits<long>::min()/10);
+    uniform_int_distribution<long>
+        ydist(numeric_limits<long>::max()/10, numeric_limits<long>::max()/2);
 
-    uniform_int_distribution<> dist2(-2, 99);
-    Point const p2(dist2(rd), 0);
-    EXPECT_EQ(p2.y(), 0);
+    long const x1 = xdist(seed);
+    long const y1 = ydist(seed);
+    Point<long> const p1(x1, y1);
+    EXPECT_EQ(p1.x(), x1);
+    EXPECT_EQ(p1.y(), y1);
 
-    uniform_int_distribution<> dist3(-100, numeric_limits<int>::min()+1);
-    Point const p3(dist3(rd), 0);
-    EXPECT_EQ(p3.y(), 0);
+    long const x2 = xdist(seed);
+    long const y2 = ydist(seed);
+    Point<long> const p2(x2, y2);
 
-    Point const p4(numeric_limits<int>::min(), 0);
-    EXPECT_EQ(p4.y(), 0);
+    auto const p12a = p1+p2;
+    EXPECT_EQ(p12a.x(), x1+x2);
+    EXPECT_EQ(p12a.y(), y1+y2);
+
+    auto const p12s = p1-p2;
+    EXPECT_EQ(p12s.x(), x1-x2);
+    EXPECT_EQ(p12s.y(), y1-y2);
 }
 
-TEST(PointTest, GetYAsPositiveInt) {
-    random_device rd;
-    uniform_int_distribution<> xdist(numeric_limits<int>::min(),
-                                     numeric_limits<int>::max());
+TEST(PointTest, XPositiveInfinityYFinite) {
+    random_device seed;
 
-    Point const p1(xdist(rd), 1);
-    EXPECT_EQ(p1.y(), 1);
+    uniform_real_distribution<long double>
+        rdist(numeric_limits<long double>::min(),
+              numeric_limits<long double>::max());
 
-    uniform_int_distribution<> ydist2(2, 99);
-    int const y2 = ydist2(rd);
-    Point const p2(xdist(rd), y2);
-    EXPECT_EQ(p2.y(), y2);
+    long double const ry1 = rdist(seed);
+    Point<long double> const rp1(numeric_limits<long double>::infinity(), ry1);
+    EXPECT_EQ(rp1.x(), numeric_limits<long double>::infinity());
+    EXPECT_EQ(norm(rp1), numeric_limits<double>::infinity());
 
-    uniform_int_distribution<> ydist3(100, numeric_limits<int>::max()-1);
-    int const y3 = ydist3(rd);
-    Point const p3(xdist(rd), y3);
-    EXPECT_EQ(p3.y(), y3);
+    long double const rx2 = rdist(seed);
+    long double const ry2 = rdist(seed);
+    Point<long double> const rp2(rx2, ry2);
 
-    // Point const p4(xdist(rd), numeric_limits<int>::max());
-    // EXPECT_EQ(p3.y(), numeric_limits<int>::max());
+    auto const rp12a = rp1+rp2;
+    EXPECT_EQ(rp12a.x(), numeric_limits<long double>::infinity());
+    EXPECT_EQ(norm(rp12a), numeric_limits<long double>::infinity());
 }
 
-TEST(PointTest, GetYAsPositiveFloat) {
-    random_device rd;
-    uniform_real_distribution<float> xdist(numeric_limits<float>::min(),
-                                           numeric_limits<float>::max());
+TEST(PointTest, XFiniteAndYNegativeInfinity) {
+    random_device seed;
 
-    uniform_real_distribution<float> ydist1(0.000001, 0.00001);
-    float const y1 = ydist1(rd);
-    Point const p1(xdist(rd), y1);
-    EXPECT_LT(p1.y()-y1, 0.00001);
+    uniform_real_distribution<float> dist(numeric_limits<float>::min(),
+                                          numeric_limits<float>::max());
 
-    uniform_real_distribution<float> ydist2(0.001, 0.999);
-    float const y2 = ydist2(rd);
-    Point const p2(xdist(rd), y2);
-    EXPECT_LT(p2.y()-y2, 0.001);
+    float const x1 = dist(seed);
+    Point<float> const p1(x1, -numeric_limits<float>::infinity());
+    EXPECT_EQ(p1.x(), x1);
+    EXPECT_EQ(p1.y(), -numeric_limits<float>::infinity());
+    EXPECT_EQ(normf(p1), numeric_limits<float>::infinity());
 
-    uniform_real_distribution<float> ydist3(1.001, 99.99);
-    float const y3 = ydist3(rd);
-    Point const p3(xdist(rd), y3);
-    EXPECT_LT(p3.y()-y3, 0.01);
+    auto const p1n = -p1;
+    EXPECT_EQ(p1n.x(), -x1);
+    EXPECT_EQ(p1n.y(), numeric_limits<float>::infinity());
+    EXPECT_EQ(norm(p1n), numeric_limits<double>::infinity());
+
+    float const x2 = dist(seed);
+    Point<float> const p2(x2, dist(seed));
+
+    auto const p12s = p1-p2;
+    EXPECT_EQ(p12s.x(), x1-x2);
+    EXPECT_EQ(p12s.y(), -numeric_limits<float>::infinity());
+    EXPECT_EQ(norm(p12s), numeric_limits<double>::infinity());
 }
 
-TEST(PointTest, GetYAsNegativeInt) {
-    random_device rd;
-    uniform_int_distribution<> xdist(numeric_limits<int>::min(),
-                                     numeric_limits<int>::max());
-
-    Point const p1(xdist(rd), -1);
-    EXPECT_EQ(p1.y(), -1);
-
-    uniform_int_distribution<> ydist2(-99, -2);
-    int const y2 = ydist2(rd);
-    Point const p2(xdist(rd), y2);
-    EXPECT_EQ(p2.y(), y2);
-
-    uniform_int_distribution<> ydist3(numeric_limits<int>::min()+1, -100);
-    int const y3 = ydist3(rd);
-    Point const p3(xdist(rd), y3);
-    EXPECT_EQ(p3.y(), y3);
-
-    Point const p4(xdist(rd), numeric_limits<int>::min());
-    EXPECT_EQ(p4.y(), numeric_limits<int>::min());
+TEST(PointTest, XisNaNAndYInfinite){
+    bool failed = false;
+    try {
+        Point<float> const p(0.0f/0.0f, numeric_limits<float>::infinity());
+    }
+    catch (...) {
+        failed = true;
+    }
+    EXPECT_TRUE(failed);
 }
 
-TEST(PointTest, GetYasNegativeFloat) {
-    random_device rd;
-    uniform_real_distribution<float> xdist(numeric_limits<float>::min(),
-                                           numeric_limits<float>::max());
+TEST(PointTest, XFiniteAndYisNaN) {
+    random_device seed;
+    uniform_real_distribution<long double>
+        dist(numeric_limits<long double>::min(),
+             numeric_limits<long double>::max());
 
-    uniform_real_distribution<float> ydist1(-0.00001, -0.000001);
-    float const y1 = ydist1(rd);
-    Point const p1(xdist(rd), y1);
-    EXPECT_LT(p1.y()-y1, 0.00001);
+    bool failed = false;
+    try {
+        Point<long double> const p(dist(seed), nanl("1"));
+    }
+    catch (...) {
+        failed = true;
+    }
+    EXPECT_TRUE(failed);
+}
 
-    uniform_real_distribution<float> ydist2(-0.999, -0.001);
-    float const y2 = ydist2(rd);
-    Point const p2(xdist(rd), y2);
-    EXPECT_LT(p2.y()-y2, 0.001);
+TEST(PointTest, XInfiniteSubInfinite) {
+    random_device seed;
+    uniform_real_distribution<double> dist(numeric_limits<double>::min(),
+                                           numeric_limits<double>::max());
 
-    uniform_real_distribution<float> ydist3(-99.99, -1.001);
-    float const y3 = ydist3(rd);
-    Point const p3(xdist(rd), y3);
-    EXPECT_LT(p3.y()-y3, 0.01);
+    Point<double> const p1(numeric_limits<double>::infinity(), dist(seed));
+    Point<double> const p2(numeric_limits<double>::infinity(), dist(seed));
+
+    bool failed = false;
+    try {
+        auto const p3 = p1 - p2;
+    }
+    catch (...) {
+        failed = true;
+    }
+    EXPECT_TRUE(failed);
 }
