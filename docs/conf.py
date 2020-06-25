1b40ff21d79d7f0abdba0987c2866a248c2fa3af
Josie Thompson <josiahst@gmail.com>
2020-06-24 21:54:25 -0700

    inspecting contents of input dir

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,10 @@ if os.environ.get('READTHEDOCS', None):
     input_exists = os.path.isdir(input_dir)
     print(f'does input directory exist? {input_exists}')
 
+    print(f'contents of {input_directory}')
+    print('\n'.join(os.listdir(input_directory)))
+    print()
+
     output_dir = 'doxygen'
     config_doxyfile(input_dir, output_dir)
     subprocess.call('doxygen', None)
@@ -40,8 +44,9 @@ if os.environ.get('READTHEDOCS', None):
     output_exists = os.path.isdir(f'{output_dir}/xml')
     print(f'does output directory exist? {output_exists}')
 
-    print(f'contents of {output_dir}')
+    print(f'contents of {output_dir}/xml')
     print('\n'.join(os.listdir(f'{output_dir}/xml')))
+    print()
 
     breathe_projects['hax'] = f'{output_dir}/xml'
 
